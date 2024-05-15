25fa91e479d292f510a5667ed2426d028dee786e
Kiarash <95994481+Ghosts6@users.noreply.github.com>
2024-05-15 17:09:43 +0330

    Create dns_flush.cmd

---
+++ cmd/dns_flush.cmd
@@ -0,0 +1,12 @@
+@echo off
+
+echo Flushing DNS resolver cache...
+
+ipconfig /flushdns
+ipconfig /registerdns
+ipconfig /release
+ipconfig /renew
+
+echo DNS resolver cache flushed successfully.
+
+echo DNS cache flush complete.
